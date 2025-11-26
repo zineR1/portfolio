36346b9c1437f6672cffe4f0c27caccffb25c8e4
zineR1 <agusdalvit@gmail.com>
2025-11-25 22:39:15 -0300

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        navbarXXL: "1260px",
-        navbarXL: "1180px",
-        navbarL: "1120px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
